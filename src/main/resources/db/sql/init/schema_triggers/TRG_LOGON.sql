2bccf2fde9f92f1eed87bac42b023c598f386c7c
Olivier Mignot <olivier.mignot83@outlook.fr>
2026-08-14 12:27:17 +0200

    correction trigger logon

--- src/main/resources/db/sql/init/schema_triggers/TRG_LOGON.sql
+++ src/main/resources/db/sql/init/schema_triggers/TRG_LOGON.sql
@@ -1,11 +1,12 @@
-CREATE OR REPLACE TRIGGER ARTHUS."TRG_LOGON"
-AFTER logon ON DATABASE
+CREATE TRIGGER arthus.trg_logon
+AFTER LOGON ON DATABASE
 BEGIN
- 
-EXECUTE IMMEDIATE 'alter session set nls_date_format=''DD-MON-RRRR''';
- 
-EXECUTE IMMEDIATE 'alter session set nls_date_language=''AMERICAN''';
- 
-EXECUTE IMMEDIATE 'alter session set nls_numeric_characters=''. ''';
- 
-END trg_logon;
+  IF SYS_CONTEXT('USERENV','SESSION_USER') IN ('SYS','SYSTEM') THEN
+    RETURN;
+  END IF;
+  EXECUTE IMMEDIATE q'[alter session set nls_date_format='DD-MON-RRRR']';
+  EXECUTE IMMEDIATE q'[alter session set nls_date_language='AMERICAN']';
+  EXECUTE IMMEDIATE q'[alter session set nls_numeric_characters='. ']';
+EXCEPTION
+  WHEN OTHERS THEN NULL;
+END;
