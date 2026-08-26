1b667fd0540638bcb2285c4222c2cecce74295e0
mrdispo <mrdispo@2022P001.local.cat-amania.com>
2026-08-26 11:59:16 +0200

    hard replace

--- src/main/java/com/arwc3/enums/StatGroup.java
+++
@@ -1,9 +0,0 @@
-package com.arwc3.enums;
-
-/** Identifie à quel graphique du tableau de bord (stat1..stat4) appartient une entrée. */
-public enum StatGroup {
-    STAT1,
-    STAT2,
-    STAT3,
-    STAT4
-}
